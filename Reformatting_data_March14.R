734b45c307c16dfe54513aec8d7c1f6702eefedf
ddlawton <ddlawton@asu.edu>
2022-03-15 16:54:15 -0400

    yeah its a pain

--- Reformatting_data_March14.R
+++ Reformatting_data_March14.R
@@ -34,7 +34,7 @@ table1 <-dat[table2_start:table2_end,] %>%
   drop_na(Pest) %>% select(2:3) %>%
   rename(Information = Pest, data = `Acres Infested`) %>%
   pivot_wider(names_from = "Information", values_from = "data") %>%
-  select(State,Region,Year,'Total Acres (Upland)','Yield / Acre (Upland)') %
+  select(State,Region,Year,'Total Acres (Upland)','Yield / Acre (Upland)') 
 
 table2 <-dat[table3_start:table3_end,] %>%
   select(`Acres Treated`,`% Acres Treated`) %>%
@@ -76,15 +76,14 @@ for (x in files4) {
     
     for (i in 1:sheetcount){
       
-      
-      State <- colnames(read_excel(x,sheet=i,.name_repair	="minimal"))[[1]]
-      Year <- colnames(read_excel(x,sheet=i,.name_repair	="minimal"))[[7]]
+      State_top1 <- colnames(read_excel(x,sheet=i,.name_repair	="minimal"))[[1]]
+      Year_top <- colnames(read_excel(x,sheet=i,.name_repair	="minimal"))[[7]]
       sheet_name <- (excel_sheets(x)[i])
       
       dat <- read_excel(x,sheet=i,skip=3) %>%
         rowid_to_column(var="id")
-  
-      table1_end <- (dat %>% filter(Pest == "TOTAL" | Pest == "Total"))$id  
+      
+      #table1_end <- (dat %>% filter(Pest == "TOTAL" | Pest == "Total"))$id  
       
       table2_start <- (dat %>% filter(Pest == "Data Input"))$id  
       table2_end <- (dat %>% filter(Pest == "% loss to other (chemical injury, weeds, diseases, etc.)"))$id  
@@ -92,10 +91,10 @@ for (x in files4) {
       table3_start <- (dat %>% filter(`Acres Treated` == "Yield and Magement Results" |`Acres Treated` ==  "Yield and Management Results" |`Acres Treated` == "Yield and Ma0gement Results"))$id  
       table3_end <- (dat %>% filter(`Acres Treated` == "Applications by Ground (acres)"))$id  
       
-      table3 <- dat[1:table1_end,] %>%
-        drop_na(Pest)  %>%
-        select(any_of(c("Pest","Acres Infested","Acres Treated")),starts_with("# of apps")) 
-        
+      #table3 <- dat[1:table1_end,] %>%
+      #  drop_na(Pest)  %>%
+      #  select(any_of(c("Pest","Acres Infested","Acres Treated")),starts_with("# of apps")) 
+      
       
       table1 <-dat[table2_start:table2_end,] %>%
         drop_na(Pest) %>% select(2:3) %>%
@@ -109,9 +108,15 @@ for (x in files4) {
         pivot_wider(names_from = `Acres Treated`, values_from = `% Acres Treated`) %>%
         select(`Total Acres`)
       
-      dat_list[[i]]  <- cbind(table1,table2,table3) %>%
-        mutate(Sheet_name = sheet_name, Sheet_Year = Year) %>% as_tibble()
+      dat_list[[i]]  <- cbind(table1,table2) %>%
+        mutate(Sheet_name = sheet_name, Sheet_Year = Year_top, State_top = State_top1) %>% as_tibble()
       
+      #table1_end <- (dat %>% filter(Pest == "TOTAL" | Pest == "Total"))$id  
+      
+      #table3 <- dat[1:table1_end,] %>%
+      #  drop_na(Pest)  %>%
+      #  select(any_of(c("Pest","Acres Infested","Acres Treated")),starts_with("# of apps")) 
+        
       
       #if (dat_list[i]$State == "2013") {  # replaces value in current row if TRUE and TRUE
       #  error_sheets[[i]] <- x}
@@ -128,12 +133,12 @@ for (x in files4) {
   }, error=function(e){cat("ERROR :",conditionMessage(e), "\n")})
 }
 
-BT_shit <- c("nonBT","BT","non-BT","bt","nonbt","non-bt","Bt", "Pima", "pima","Conventional","conventional")
+#BT_shit <- c("nonBT","BT","non-BT","bt","nonbt","non-bt","Bt", "Pima", "pima","Conventional","conventional")
 
 
 pest_pressure_data <- bind_rows(overall_dat_list, .id = "column_label")  %>% 
-  filter(!grepl(paste(BT_shit, collapse="|"), Sheet_name),
-         Pest != "TOTAL") %>%
+  filter(!grepl(paste(BT_shit, collapse="|"), Sheet_name)) %>%#,
+        # Pest != "TOTAL") %>%
   mutate(Region = case_when(
           State == "2013" ~ "Coast & Blacklands",
           TRUE ~ Region), 
@@ -145,8 +150,17 @@ pest_pressure_data <- bind_rows(overall_dat_list, .id = "column_label")  %>%
           TRUE ~ State))
 
 
-unique(levels(factor(pest_pressure_data$State)))
-unique(levels(factor(pest_pressure_data2$Sheet_name)))
+NA_Dat <- pest_pressure_data[!complete.cases(pest_pressure_data$State), ] %>%
+  filter(State_top %in% names) %>%
+  mutate(State = State_top, Year = Sheet_Year)
+
+
+pest_pressure_data.5 <- pest_pressure_data %>%
+  drop_na(State) %>%
+  rbind(NA_Dat)
+
+unique(levels(factor(pest_pressure_data.5$State)))
+unique(levels(factor(pest_pressure_data.5$Sheet_state_top)))
 
 AZ_fix <- c("Arizo","Arizo0")
 LA_fix <- c("Louisia","Louisia0")
@@ -157,7 +171,7 @@ TX_fix <- c("Texs","texas")
 
 remove_regions <- c("West","US","Central","Midsouth","Sheet41","Sheet34","Southeast")
 
-pest_pressure_data2 <- pest_pressure_data %>%
+pest_pressure_data2 <- pest_pressure_data.5 %>%
   mutate(State = case_when(
     State %in% AZ_fix ~ "Arizona",
     State %in% LA_fix ~ "Louisiana",
@@ -165,6 +179,14 @@ pest_pressure_data2 <- pest_pressure_data %>%
     State %in% SC_fix ~ "South Carolina",
     State %in% VA_fix ~ "Virginia",
     State %in% TX_fix ~ "Texas",
+    State_top %in% AZ_fix ~ "Arizona",
+    State_top %in% LA_fix ~ "Louisiana",
+    State_top %in% NC_fix ~ "North Carolina",
+    State_top %in% SC_fix ~ "South Carolina",
+    State_top %in% VA_fix ~ "Virginia",
+    State_top %in% TX_fix ~ "Texas",
+    State_top == "Tennessse" ~ "Tennessee",
+    State == "Tennessse" ~ "Tennessee",
     TRUE ~ State
   ),Year = case_when(
     Year == "184" ~ "1984",
@@ -172,12 +194,65 @@ pest_pressure_data2 <- pest_pressure_data %>%
     TRUE ~ Year
   )) %>% 
   mutate(across(where(is.character), str_squish)) %>%
-  filter(Year > 1970)
+  filter(Year > 1970, State %ni% remove_regions) %>% 
+  drop_na(`Total Acres`) %>%
+  clean_names() %>%
+  
+  #Sorry I messed this up
+  mutate(across(total_acres_upland:total_acres,as.numeric),
+         across(street_name:state_top,as.factor),
+                total_acres_pima = as.numeric(total_acres_pima),
+          state = factor(state))
 
 
 names <- state.name
 
-view(pest_pressure_data2 %>% filter(Year < 1979))
+view(pest_pressure_data2 %>% filter(Year == 2009))
+
+
+pest_pressure_data2 <- pest_pressure_data2 %>% mutate(State = case_when(
+  state == "Texas" & sheet_name == "Virginia" ~ "Virginia",
+  TRUE ~ state
+)) 
+
+
+#something is weird about texas. Lets look at it
+###THIS IS WHERE I LEFT OFF. I am trying to summarize the 1987 texas data to add to the overall dataframe
+
+
+
+
+TX_dat <- pest_pressure_data2 %>% filter(state=="Texas") %>%
+  mutate(year = as.integer(year), sheet_year = as.integer(sheet_year))
+
+
+TX_1987 <- TX_dat %>% filter(year == 1987) %>% 
+  group_by(state,year) %>%
+  summarise(total_acres_upland = sum(total_acres_upland),
+            yield_acre_upland = sum(yield_acre_upland),
+            yield_potential_lb_acre = sum(yield_potential_lb_acre, na.rm=TRUE),
+            yield_acre_pima = sum(yield_acre_pima, na.rm=TRUE),
+            sheet_year = first(sheet_year),
+            sheet_name = first(sheet_name),
+            state_top = first(state_top),
+            total_acres_pima = sum(total_acres_pima,na.rm = TRUE))
+
+TX_dat %>%
+  mutate(total_acres = as.numeric(`Total Acres (Upland)`)) %>%
+  filter(n=sum(total_acres))
+
+
+
+
+
+
+
+# Will need to address this later....
+other_dat <- pest_pressure_data2 %>% filter(State !="Texas")
+
+
+
+
 
 to_filter <- pest_pressure_data2 %>% 
        group_by(Year,State) %>% 
@@ -191,35 +266,68 @@ to_filter <- unique(to_filter$year_state)
 no_filtering_needed <- pest_pressure_data2 %>%
   mutate(Year = as.integer(as.numeric(Year)),
          year_state = paste0(Year,"_",State)) %>%
-  filter(year_state %ni% to_filter)
+  filter(year_state %ni% to_filter) %>% droplevels()
+
+
+unique(levels(factor(filtering_needed$Sheet_name)))
+
 
 
 filtering_needed <- pest_pressure_data2 %>%
   mutate(Year = as.integer(as.numeric(Year)),
          year_state = paste0(Year,"_",State)) %>%
   filter(year_state %in% to_filter,
-         Sheet_name %ni% names)
+         Sheet_name %in% names)
+
+
+
 
 filtered_dat <- rbind(no_filtering_needed,filtering_needed) %>%
   mutate(across(`Total Acres (Upland)`:`Total Acres`,as.numeric),
-         across(`Acres Infested`:`# of apps /acres treated`,as.numeric),
          `Total Acres (Pima)` = as.numeric(`Total Acres (Pima)`),
          State = factor(State),
          Region = factor(Region),
          Sheet_name = factor(Sheet_name),
          Sheet_Year = as.integer(Sheet_Year),
-         column_label = as.integer(column_label))
+         column_label = as.integer(column_label)) %>% droplevels()
+
+NA_Dat <- filtered_dat[!complete.cases(filtered_dat$State), ]
 
 
-view(filtered_dat %>% group_by(State,Year) %>%
-  summarise(sum = sum(`Total Acres`,na.rm=TRUE)))
+test <- (filtered_dat %>% group_by(State,Year) %>%
+  summarise(sum = mean(`Total Acres`,na.rm=TRUE)))
+
+summary(test)
+
+levels(unique(filtered_dat$State))
 
 
+test <- filtered_dat %>% 
+  group_by(State,Year) %>%
+  summarise(n=n())
 
-cleaned <- filtered_dat %>% group_by(State,Year) %>%
-  summarize(Total_acres=sum(`Total Acres`),
-            Yield = sum(`Yield / Acre (Upland)`),
+
+unique(levels(factor(filtered_dat$State_top)))
+
+cleaned <- filtered_dat %>% 
+  group_by(State_top,Year) %>%
+  summarize(Total_acres=mean(`Total Acres`,na.rm=TRUE),
+            Yield = mean(`Yield / Acre (Upland)`,na.rm=TRUE),
+            Yiel_potenital = mean(`yield potential (lb/acre)`,na.rm=TRUE),
             )
+
+summary(cleaned$Yiel_potenital)
+
+ggplot(cleaned,aes(x=Yiel_potenital)) + geom_histogram()
+
+
+
+cleaned %>%
+  filter(State == "Texas") %>%
+ggplot(aes(x=factor(Year),y=Total_acres,color=State)) + geom_point() + geom_line()
+
+
+
 
 ### Now combining with the dataset produced by anders
 
