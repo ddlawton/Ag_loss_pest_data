ad50cd80be82a77a7c98856345719bfe1d940a48
Douglas Lawton <ddlawton@ncsu.edu>
2022-03-22 08:25:29 -0400

    monitors acting up -- need to restrt

--- code/Exploring_the_pest_pressure_data.R
+++ code/Exploring_the_pest_pressure_data.R
@@ -8,6 +8,7 @@ library(tidyverse)
 library(ggridges)
 library(janitor)
 library(mgcv)
+library(DHARMa)
 
 
 
@@ -16,11 +17,11 @@ dat <- read_csv("data/processed/cleaned_ag_pest_data_March18.csv") %>%
   clean_names() %>%
   select(!c(x1,x2)) %>%
   mutate(across(where(is.character), as.factor),
-         toxin = case_when(
+         toxin = factor(case_when(
            year < 1996 ~ "pre_BT",
            year >= 1996 & year <= 2006 ~ "Transition",
            year > 2006 ~ "BT_era"
-         )) 
+         ))) 
   
 ggplot(dat,aes(x=percent_acres_infested)) + geom_histogram()
 
@@ -102,40 +103,93 @@ names <- c("")
 
 #just some quick modeling
 
-comparison <- dat2 %>% filter(pest %in% names)
 
-ggplot(comparison,aes(x=year2,y=percent_acres_infested,color=pest)) + geom_smooth() + ggpubr::theme_pubr()
+dat <- dat %>%
+  mutate(percent_adj = (percent_acres_infested*(33712-1)+0.5)/33712,
+         BT_group = factor(paste0(toxin,"_",group)),
+         acres_infested_int = (acres_infested)+1)
+
+unique(dat$BT_group)
+?bam
 
 
-mod <- gam(percent_acres_infested~te(year,group,bs="fs",m=2,k=20) + s(state,bs="re") + s(total_acres),select=TRUE,family=scat(),data=dat)
+view(dat %>%
+  filter(year  >= 1994, group == "boll weevil",state == "Texas") %>% 
+  arrange(year))
 
-mod_by <- gam(percent_acres_infested~te(year2,by=pest,bs="tp",m=2,k=20) + s(region,bs="re"),select=TRUE,family=scat(),data=comparison)
+unique(tx_boll$sheet_name)
+
+
+
+
+dat %>%
+  filter(group == "boll weevil",state == "Texas") %>% 
+  ggplot(aes(x=as.integer(year),y=acres_infested,color=group)) + 
+  scale_y_continuous(labels = scales::comma) +
+  scale_x_continuous(breaks = seq(1980,2021,by=4)) +
+  geom_smooth(method="gam") +
+  geom_point() +
+  ggpubr::theme_pubr() +
+  ylab("Acres infested") + 
+  xlab("Year")
+  
+mod <- bam(acres_infested   ~
+             #te(year,group,bs="fs",m=2,k=20) +
+             te(year,state,group,bs=c("tp","re","re"),m=2,k=20) +
+             s(region,state,bs="re") + 
+             s(group,bs="re") +
+             s(total_acres,k=25),
+           select=TRUE,family=tw(), data=dat,discrete = TRUE,nthreads = 23)
+
+aov_mod <- aov(percent_adj~BT_group,data=dat)
+summary(aov_mod)
+TukeyHSD(aov_mod)
+
+
+mod_by <- bam(percent_adj  ~
+             te(year,by=group,bs="tp",k=20) +
+             s(region,state,bs="re") + 
+             s(group,bs="re") +
+             s(total_acres,k=25),
+           select=TRUE,family=betar(),data=dat,discrete = TRUE,nthreads = 23)
+
+
+mod_by <- bam(acres_infested ~ te(year,by=group,bs="tp",m=2,k=20)  + s(state,bs="re") + s(total_acres,k=20),select=TRUE,family=scat(),
+              data=dat,discrete = TRUE,nthreads = 8)
 
 AIC(mod,mod_by)
 
+test <- gratia::smooth_estimates(mod,type="link")
+
 summary(mod)
 k.check(mod)
-gratia::appraise(mod)
+sim_resid <- simulateResiduals(mod_by)
+plot(sim_resid)
+
+
 output <- gratia::smooth_estimates(mod)
 
 b0 <- coef((mod))[1]
 
 test <- gratia::smooth_estimates((mod))
 
-test$adj_est <- test$est + b0
+test$adj_est <- test$est 
 
-date_trt <- test %>%
-  drop_na(pest) %>%
-  ggplot(aes(x=year2,group=pest)) + #geom_ribbon(aes(ymin=adj_est-se, ymax=adj_est+se,fill=pest),alpha=.1) +
-  geom_line(aes(y = adj_est, color=pest),size=1.5) + ggpubr::theme_pubr() +
-  scale_color_manual(values = c("#5b588f", "#f1c039", "#a21fa1", "#53a4e5")) +
-  scale_fill_manual(values = c("#5b588f", "#f1c039", "#a21fa1", "#53a4e5")) + ggtitle("Pest Pressure through the years") + xlab("Year") + ylab("Area Infested (%)")
+group_year <- test %>%
+  filter(smooth == "te(year,state,group)") %>%
+  ggplot(aes(x=year,group=group)) + geom_ribbon(aes(ymin=adj_est-se, ymax=adj_est+se,fill=group),alpha=.1) +
+  geom_line(aes(y = adj_est, color=group),size=1.5) + ggpubr::theme_pubr() +
+  scale_color_manual(values = c("#58b5e1", "#b11478", "#9bea30", "#4224bf", "#749d3c", "#fa7ee3", "#19a71f", "#e313ee", "#09f54c")) +
+  scale_fill_manual(values = c("#58b5e1", "#b11478", "#9bea30", "#4224bf", "#749d3c", "#fa7ee3", "#19a71f", "#e313ee", "#09f54c")) + 
+  ggtitle("Pest Pressure through the years") + xlab("Year") + ylab("Area Infested (%)") + 
+  facet_wrap(state~group,scales="free") 
 
-date_block <- test %>% filter(smooth == "te(Date_numeric,Block)") %>%
-  ggplot(aes(x=anytime::anytime(Date_numeric),group=Block)) + geom_ribbon(aes(ymin=adj_est-se, ymax=adj_est+se,fill=Block),alpha=.1) +
-  geom_line(aes(y = adj_est, color=Block)) + theme_pubr() +
-  scale_color_manual(values = c("#003f5c","#7a5195","#ef5675","#ffa600")) +
-  scale_fill_manual(values = c("#003f5c","#7a5195","#ef5675","#ffa600")) + ggtitle("date by block smooth")+ xlab("Date")
+total_acres <- test %>%
+  filter(smooth == "s(total_acres)") %>%
+  ggplot(aes(x=total_acres)) + geom_ribbon(aes(ymin=adj_est-se, ymax=adj_est+se),alpha=.1) +
+  geom_line(aes(y = adj_est),size=1.5) + ggpubr::theme_pubr() +
+  ggtitle("acres infested x total acres") + xlab("total acres") + ylab("Estimate")
+
 
 mod_trt <- test %>% filter(smooth == "te(MOD,Trt)") %>%
   ggplot(aes(x=(MOD/60),group=Trt)) + geom_ribbon(aes(ymin=adj_est-se, ymax=adj_est+se,fill=Trt),alpha=.1) +
